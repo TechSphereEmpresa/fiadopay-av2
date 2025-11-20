a81bf42854495837e7079f7c6425903070cf804c
AlannaFM <alannamachado84@gmail.com>
2025-11-20 11:35:31 -0300

    feat: Criando anotações

---
+++ src/main/java/edu/ucsal/fiadopay/annotations/AntiFraud.java
@@ -0,0 +1,17 @@
+package edu.ucsal.fiadopay.annotations;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AntiFraud {
+	String name(); //ex: HighAmount
+	double threshold(); //ex: valor mínimo para bloquear
+}
